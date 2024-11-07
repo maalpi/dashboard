55c23fcad8030281d044bfbef828cb18a3bab1a5
maalpi <mateus.mateus30@gmail.com>
2024-11-07 15:47:43 -0300

    feat: criando dialogo para apagar evento

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -7,6 +7,20 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import brLocale from '@fullcalendar/core/locales/pt-br';
 import { useEffect, useState } from 'react';
 
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 interface Event {
     title: string;
     start: Date | string;
@@ -49,6 +63,12 @@ export default function Calendario() {
             })
         }
     }, [])
+
+    function handleDelete() {
+        setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)))
+        setShowDeleteModal(false)
+        setIdToDelete(null)
+      }
 
     function handleDateClick(arg: {date: Date, allDay: boolean}) {
         setNewEvent({
@@ -115,7 +135,27 @@ export default function Calendario() {
                 </div>
             </div>
 
-            <Transition
+            <Dialog open={showDeleteModal} onOpenChange={setShowDeleteModal}>
+                <DialogContent className="sm:max-w-md sm:justify-center">
+                    <DialogHeader>
+                    <DialogTitle className='text-center'>Apagar evento</DialogTitle>
+                    <DialogDescription className='text-center'>
+                        você tem certeza que quer apagar esse evento?
+                    </DialogDescription>
+                    </DialogHeader>            
+                    <DialogFooter className="sm:justify-center">
+
+                            <Button type="button" variant="destructive" onClick={handleDelete}>
+                                Apagar
+                            </Button>
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Cancelar
+                        </Button>
+                    </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </main>
     )
 }
