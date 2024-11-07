32d540a12467d0e22fdc5202057e23f8ca50c054
maalpi <mateus.mateus30@gmail.com>
2024-11-07 15:14:15 -0300

    feat: arrastando eventos para dias do calendario

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -5,7 +5,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid';
 import brLocale from '@fullcalendar/core/locales/pt-br';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Event {
     title: string;
@@ -20,6 +20,7 @@ export default function Calendario() {
         {title: 'event 2', id: '2'},
         {title: 'event 3', id: '3'},
         {title: 'event 4', id: '4'},
+        {title: 'event 5', id: '5'},
     ])
 
     const [allEvents, setAllEvents] = useState<Event[]>([])
@@ -32,6 +33,49 @@ export default function Calendario() {
         allDay: false,
         id:0
     })
+
+    useEffect(() => {
+        let draggableEl = document.getElementById('draggable-el');
+
+        if (draggableEl) {
+            new Draggable(draggableEl, {
+                itemSelector: '.fc-event',
+                eventData: function (eventEl) {
+                    let title = eventEl.getAttribute('title')
+                    let id = eventEl.getAttribute('data')
+                    let start = eventEl.getAttribute('start')
+                    return {title, id, start}
+                }
+            })
+        }
+    }, [])
+
+    function handleDateClick(arg: {date: Date, allDay: boolean}) {
+        setNewEvent({
+            ...newEvent,
+            start: arg.date,
+            allDay: arg.allDay,
+            id: new Date().getTime()
+        })
+        setShowModal(true);
+    }
+
+    function addEvent(data: DropArg) {
+        const event = { 
+            ...newEvent, 
+            start: data.date.toISOString(), 
+            title: data.draggedEl.innerText, 
+            allDay: data.allDay, 
+            id: new Date().getTime() 
+        }
+        setAllEvents([...allEvents, event])
+    }
+
+    function handleDeleteModal(data: {event: {id:string}}) {
+        setShowDeleteModal(true);
+        setIdToDelete(Number(data.event.id));
+    }
+
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-16">
@@ -54,15 +98,15 @@ export default function Calendario() {
                                 droppable={true}
                                 selectable={true}
                                 selectMirror={true}
-                                //   dateClick={{}}
-                                //   drop={}
-                                //   eventClick={}
+                                dateClick={handleDateClick}
+                                drop={(data) => addEvent(data)}
+                                eventClick={(data) => handleDeleteModal(data)}
                     />
                     </div>
                     <div id='draggable-el' className='ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-200'>
                         <h1 className='font-bold text-lg text-center'>Drag event</h1>
                         {events.map(e => (
-                            <div key={e.title} 
+                            <div key={e.id} 
                                  className='fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white text-violet-700'
                                  title={e.title}>
                                     {e.title}
@@ -70,6 +114,8 @@ export default function Calendario() {
                         ))}
                 </div>
             </div>
+
+            <Transition
         </main>
     )
 }
