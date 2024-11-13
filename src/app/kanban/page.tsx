feef84a014c3bde860d69c9fdfb6fc9a2362ba5c
maalpi <mateus.mateus30@gmail.com>
2024-11-13 15:57:11 -0300

    feat: adicionando o botão de apagar a tabela kanban

--- src/app/kanban/page.tsx
+++ src/app/kanban/page.tsx
@@ -2,13 +2,16 @@
 import { useEffect, useState } from "react";
 import { KanbanDialog } from "@/components/dialog/kanbanDialog";
 import { db } from "@/db/firebase"; // Importar o db do seu Firebase
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
+import { Trash2 } from "lucide-react";
 
 export default function Home() {
-  const [kanbanTables, setKanbanTables] = useState([]); // Estado para armazenar as tabelas
-  const router = useRouter(); // Inicialize o router
+  const [kanbanTables, setKanbanTables] = useState([]); 
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+
+  const router = useRouter(); 
 
   // Função para buscar os dados do Firestore
   const fetchKanbanTables = async () => {
@@ -23,6 +26,19 @@ export default function Home() {
   useEffect(() => {
     fetchKanbanTables(); // Chama a função ao montar o componente
   }, []);
+
+
+  const handleDelete = async (id) => {
+    const confirmed = window.confirm("Tem certeza que deseja deletar este item?");
+    if (confirmed) {
+      try {
+        await deleteDoc(doc(db, "kanbanTables", id));
+        setKanbanTables((prevTables) => prevTables.filter((table) => table.id !== id));
+      } catch (error) {
+        console.error("Erro ao deletar o item:", error);
+      }
+    }
+  };
 
   const handlePage = (tableName) => {
     router.push(`/tabela/${tableName}`);
@@ -40,8 +56,23 @@ export default function Home() {
             key={table.id} 
             className="sm:w-[200px] p-4 border rounded-md cursor-pointer hover:bg-gray-200 transition-all hover:text-black" 
             onClick={() => handlePage(table.name)}
-        > 
-            <CardTitle className="transition-all">{table.name}</CardTitle>
+            onMouseEnter={() => setMouseIsOver(true)}
+            onMouseLeave={() => setMouseIsOver(false)}
+        >   
+            <div className="break-words flex flex-row">
+              <CardTitle className="transition-all w-44">{table.name}</CardTitle>
+              {mouseIsOver && (
+              <button
+                onClick={(e) => {
+                  e.stopPropagation(); // Impede que o clique abra a página
+                  handleDelete(table.id);
+                }}
+                className="float-right ml-4 bg-transparent border-none hover:bg-transparent ease-in"
+              >
+                <Trash2 size={17} className="text-red-500 hover:text-red-950 ease-in" />
+              </button>
+            )}
+            </div>
             <CardDescription className="mt-3">{table.imageUrl}</CardDescription> {/* Exibir a descrição ou a URL da imagem */}
           </Card>
         ))}
